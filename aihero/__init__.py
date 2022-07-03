aed4dddbc91cf2c6017a9287ba48fff1b6978b16
Rahul Parundekar <rparundekar@gmail.com>
2022-07-03 13:39:37 -0700

    Ping works.

--- aihero/__init__.py
+++ aihero/__init__.py
@@ -1,17 +1,31 @@
 from aihero.exceptions import AIHeroException
 from .api import Api
 from .exceptions import AIHeroException
+from .automation import Automation
+from .detect_sentiment import DetectSentiment
 
 
 class Client:
     """Main sync client class to talk to AI Hero"""
 
     def __init__(self, api_key=None, server_url=None):
-        self._server_url = server_url
         self._api = Api(api_key=api_key, server_url=server_url)
 
     def ping(self):
         return self._api.get(
             self._api.ping_endpoint(),
-            error_msg=f"Could not connect to the server {self._server_url}",
+            error_msg=f"Could not connect to the server {self._api.server_url}",
         )
+
+    def get_automation(self, automation_id, api_key):
+        automation = Automation(
+            automation_id=automation_id,
+            api=Api(api_key=api_key, server_url=self._api.server_url),
+        )
+        automation_definition = automation.get_definition()
+        if automation_definition["type"] == "detect_sentiment":
+            automation = DetectSentiment(
+                automation_id=automation_id,
+                api=Api(api_key=api_key, server_url=self._api.server_url),
+            )
+        return automation
--- aihero/api.py
+++ aihero/api.py
@@ -14,28 +14,53 @@ class Api:
         self._headers = {
             "Content-Type": "application/json",
         }
-        if api_key == "" or api_key is None:
+        if api_key is None or api_key.strip() == "":
             self._api_key = None  # Only works for ping for now.
         else:
             self._api_key = api_key
             self._authorization = f"Bearer {self._api_key}"
             self._headers["Authorization"] = self._authorization
 
-        self._server_url = server_url or PRODUCTION_URL
+        if server_url.endswith("/"):
+            server_url = server_url[:-1]
+        self.server_url = server_url or PRODUCTION_URL
 
     def ping_endpoint(self):
-        return f"{self._server_url}/ping"
+        return f"{self.server_url}/ping"
 
     def endpoint(self, *args):
-        return f"{self._server_url}/api/{'/'.join(args)}"
+        return f"{self.server_url}/api/{'/'.join(args)}"
 
-    def get(self, path, error_msg=None):
+    def get(self, path, error_msg=None, network_errors=None):
         with httpx.Client() as client:
             response = client.get(path, headers=self._headers)
         try:
             response.raise_for_status()
         except httpx.HTTPStatusError as exc:
-            raise AIHeroException(
-                f"Error {error_msg or path}", status_code=exc.response.status_code
-            )
+            if network_errors and exc.response.status_code in network_errors:
+                raise AIHeroException(
+                    f"{network_errors[exc.response.status_code]}",
+                    status_code=exc.response.status_code,
+                )
+            elif exc.response.status_code:
+                raise AIHeroException(error_msg, status_code=exc.response.status_code)
+            else:
+                raise AIHeroException(error_msg)
+        return response.json()
+
+    def post(self, path, obj, error_msg=None, network_errors=None):
+        with httpx.Client() as client:
+            response = client.post(path, json=obj, headers=self._headers)
+        try:
+            response.raise_for_status()
+        except httpx.HTTPStatusError as exc:
+            if network_errors and exc.response.status_code in network_errors:
+                raise AIHeroException(
+                    f"{network_errors[exc.response.status_code]}",
+                    status_code=exc.response.status_code,
+                )
+            elif exc.response.status_code:
+                raise AIHeroException(error_msg, status_code=exc.response.status_code)
+            else:
+                raise AIHeroException(error_msg)
         return response.json()
